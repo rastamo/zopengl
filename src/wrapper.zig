aaf7b3a0cecb64cfa3f057248c5e71431cbd803b
Joakim Rastamo <rastamo@vivaldi.net>
2026-04-30 18:44:12 +0200

    Update to Zig 0.16.0

--- src/wrapper.zig
+++ src/wrapper.zig
@@ -10,27 +10,27 @@ const meta = struct {
         const num_fields = countFields: {
             var count: comptime_int = 0;
             for (Enums) |Subset| {
-                for (std.meta.fields(Subset)) |_| count += 1;
+                count += @typeInfo(Subset).@"enum".fields.len;
             }
             break :countFields count;
         };
-        comptime var fields: [num_fields]std.builtin.Type.EnumField = .{undefined} ** num_fields;
+        comptime var names: [num_fields][]const u8 = undefined;
+        comptime var values: [num_fields]tag_type = undefined;
         comptime var i = 0;
-        for (Enums) |Subset| {
-            const subset_info = @typeInfo(Subset).@"enum";
-            assert(subset_info.tag_type == tag_type);
-            for (subset_info.fields) |field| {
-                assert(i < fields.len);
-                fields[i] = field;
+        inline for (Enums) |Subset| {
+            const info = @typeInfo(Subset).@"enum";
+            inline for (info.fields) |field| {
+                names[i] = field.name;
+                values[i] = field.value;
                 i += 1;
             }
         }
-        return @Type(.{ .@"enum" = .{
-            .tag_type = tag_type,
-            .fields = &fields,
-            .decls = &.{},
-            .is_exhaustive = true,
-        } });
+        return @Enum(
+            tag_type,
+            .exhaustive,
+            &names,
+            &values,
+        );
     }
 };
 
--- src/zopengl.zig
+++ src/zopengl.zig
@@ -6,7 +6,7 @@ const build_options = @import("build_options");
 
 comptime {
     @setEvalBranchQuota(20_000);
-    _ = std.testing.refAllDeclsRecursive(@This());
+    _ = std.testing.refAllDecls(@This());
 }
 
 pub const bindings = @import("bindings.zig");
